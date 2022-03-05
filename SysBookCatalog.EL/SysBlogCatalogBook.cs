ea00e2dba9e2a5c2099ea3d498bec3d96844c3d2
Mclovin <anonimomclovin341@gmail.com>
2022-03-05 09:50:00 -0600

    New Class

---
+++ SysBookCatalog.EL/SysBlogCatalogBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace SysBookCatalog.EL
+{
+    internal class SysBlogCatalogBook
+    {
+        [Key]
+        private int IdUser { get; set; }
+        private string LevelUser { get; set; }
+        [Required]
+        [StringLength(100)]
+        private DateTime DateCreateUser { get; set; }
+        [Required]
+        [StringLength(50)]
+        private string Name { get; set; }
+        internal string Email { get; set; }
+        internal string Password { get; set; }
+        private Boolean State { get; set; }
+
+
+        public void ReadBook() { 
+        }
+        public void PlaysAudioBook() { 
+        }
+
+        public void HistoryBookCap() { 
+        }
+        public void CalificaLibro()
+        {
+
+        }
+
+
+    }
+}
---
+++ SysBookCatalog.EL/SysBookCatalogUser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace SysBookCatalog.EL
+{
+    internal class SysBookCatalogUser
+    {
+        [Key]
+        private int IdBook { get; set; }
+        private string title { get; set; }
+        private DateTime Lounch { get; set; }
+        private string Editorial { get; set; }
+        private int NPages { get; set; }
+        private Boolean State { get; set; }
+
+
+        public void GiveALike()
+        {
+
+        }
+
+        public void Comment()
+        {
+
+        }
+
+        public void GradeBook()
+        {
+
+        }
+
+        public void AplicPromotion()
+        {
+
+        }
+
+        public void MakeAClaim()
+        {
+
+        }
+    }
+}
